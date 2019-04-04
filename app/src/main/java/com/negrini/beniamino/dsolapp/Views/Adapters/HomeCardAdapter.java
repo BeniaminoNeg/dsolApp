7165cc3001f39656d8d221a1f2e2de3e169013cf
Beniamino Negrini <beniamino.negrini@gmail.com>
2019-04-04 16:34:07 +0200

    adapter card

--- app/src/main/java/com/negrini/beniamino/dsolapp/Views/Adapters/HomeCardAdapter.java
+++ app/src/main/java/com/negrini/beniamino/dsolapp/Views/Adapters/HomeCardAdapter.java
@@ -1,7 +1,14 @@
 package com.negrini.beniamino.dsolapp.Views.Adapters;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.negrini.beniamino.dsolapp.R;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class HomeCardAdapter extends RecyclerView.Adapter<HomeCardAdapter.HomeViewHolder> {
 
@@ -12,10 +19,20 @@ public class HomeCardAdapter extends RecyclerView.Adapter<HomeCardAdapter.HomeVi
         public MyViewHolder(TextView v){
             super(v);
             mookText = v;
+
         }
     }
 
     public HomeCardAdapter (String [] myDataset){
         mDataset = myDataset;
+    }
+
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+
+        // Sto creando una nuova view
+        TextView v = (TextView) LayoutInflater.from(parent.getContext()).inflate(R.layout.my_text_view, parent, false);
+        //...
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
     }
 }
